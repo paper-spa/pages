6a21fac1782037048863c0d41ab611dbbf9d9caa
Jesse Riggins <jriggins@users.noreply.github.com>
2021-11-04 11:30:51 -0500

    Modify entrypoint to call github-pages executable instead of jekyll.
    Add verbose support

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-cd /src/gh/pages-gem
+#######################################################################################################
+# 
+# Calls github-pages executable to build the site using whitelisted plugins and supported configuration
+#
+#######################################################################################################
+
+set -o errexit
 
 SOURCE_DIRECTORY=${GITHUB_WORKSPACE}/$INPUT_SOURCE
 DESTINATION_DIRECTORY=${GITHUB_WORKSPACE}/$INPUT_DESTINATION
+PAGES_GEM_HOME=/src/gh/pages-gem
+GITHUB_PAGES=$PAGES_GEM_HOME/bin/github-pages
 
-jekyll build --verbose --source $SOURCE_DIRECTORY --destination $DESTINATION_DIRECTORY
+if [ -z $INPUT_VERBOSE ]; then
+  VERBOSE=''
+else
+  VERBOSE='--verbose'
+fi
+
+cd $PAGES_GEM_HOME
+
+$GITHUB_PAGES build $VERBOSE --source $SOURCE_DIRECTORY --destination $DESTINATION_DIRECTORY
