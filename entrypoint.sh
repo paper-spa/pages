8304162851b45ea1a70150b6206436af410d8516
Jesse Riggins <jriggins@users.noreply.github.com>
2021-11-04 12:36:06 -0500

    Temporarily comment github token variable.

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ PAGES_GEM_HOME=/src/gh/pages-gem
 GITHUB_PAGES=$PAGES_GEM_HOME/bin/github-pages
 
 # Set environment variables required by supported plugins
-export JEKYLL_GITHUB_TOKEN=$GITHUB_TOKEN
+#export JEKYLL_GITHUB_TOKEN=$GITHUB_TOKEN
 export PAGES_REPO_NWO=$GITHUB_REPOSITORY
 
 if [ -z $INPUT_VERBOSE ]; then
